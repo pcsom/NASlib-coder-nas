fbe6ea1b6751ef96f1ef3665e47fe74f229aae7e
Pranav Somu <pranavcsomu@gmail.com>
2026-01-12 04:08:39 -0500

    Add seed argument to submission script

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=codenas
 #SBATCH --nodes=1
-#SBATCH -c 24
-#SBATCH --time=10:00:00
+#SBATCH -c 20
+#SBATCH --time=5:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
-#SBATCH --output=slurm_logs/naslib_%j.out
+#SBATCH --output=slurm_logs/naslib301_%j.out
+
+# Accept seed as first argument, default to 242 if not provided
+SEED=${1:-242}
 
 nvidia-smi
 module load anaconda3/2023.03
-conda run -n naslib39v2 --no-capture-output python -u run_nb201_comparison.py
+conda run -n naslib39v2 --no-capture-output python -u run_nb301_comparison.py --seed $SEED
